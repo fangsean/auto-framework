b1f7b439e1ff6fcc79b7231d6cb63c189814a2f6
jsen.yin <jsen.yin@gmail.com>
2019-03-16 19:12:32 +0800

    more

--- src/test/java/com/auto/test/DataCount.java
+++ src/test/java/com/auto/test/DataCount.java
@@ -2,7 +2,47 @@ package com.auto.test;
 
 public class DataCount {
 
+    private static /*volatile*/ int count;
 
+    public static void main(String[] args) {
+
+        count = 0;
+
+        for (int i = 0; i < 1000; i++) {
+            Thread thread = new Thread(() -> {
+                count++;
+            });
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(count);
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(()->{
+            System.out.println("============");
+        }).start();
+
+        int[][][][][] ints = new int[2][2][2][3][3];
+
+
+    }
 
 
 }

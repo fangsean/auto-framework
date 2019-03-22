dcb72c255df09febbf9b581488359aded68ae84a
jsen.yin <jsen.yin@gmail.com>
2019-03-22 13:18:51 +0800

    more

--- src/test/java/com/auto/test/AppTest.java
+++ src/test/java/com/auto/test/AppTest.java
@@ -4,4 +4,14 @@ package com.auto.test;
  * Created by jobob on 17/5/16.
  */
 public class AppTest {
+
+    public static void main(String[] args) {
+
+        for (int i=0;i<10000000;i++){
+            new Thread(()->{
+                System.out.println(System.currentTimeMillis());
+            }).start();
+        }
+    }
+
 }
